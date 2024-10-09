11b54fc87df3883ea8b2790fb142c0b1bb77b67e
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2024-10-09 12:53:28 +0100

    added bootstrap and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          coded by
+          <a
+            href="https://github.com/Moredebbs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Moyinoluwa Owoeye
+          </a>
+          is
+          <a
+            href="https://github.com/Moredebbs/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+          and
+          <a
+            href="https://react-mi-clima.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
